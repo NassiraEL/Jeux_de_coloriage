3a2e35300a35674437c9b9211bfdd1ebdaada1d9
Nassira EL <nassirans01@gmail.com>
2023-10-23 09:43:39 +0100

    ajouter un effet de glissement (swipe)

--- coloriageJs.js
+++ coloriageJs.js
@@ -18,7 +18,7 @@ letters.forEach(letter=>{
     })
     letter.addEventListener("dragleave", function(e){
        // letter.style.background =  "#fff";
-       letter.style.borderColor =   "black";
+       letter.style.borderColor =   "#FFA600";
         e.preventDefault();
     })
 
@@ -27,3 +27,32 @@ letters.forEach(letter=>{
        letter.style.borderColor =   drag;
     })
 })
+
+//////////////////////////swiper letters
+const tabs = document.querySelectorAll('.tab');
+const preButton = document.getElementById('pre');
+const nextButton = document.getElementById('next');
+let currentIndex = 0;
+
+function showLetter(index) {
+    tabs.forEach((tab, i) => {
+        const offset = i - index;
+        tab.style.transform = `translateX(${offset * 120}px)`; // Ajustez la valeur de translation selon vos besoins
+    });
+}
+
+preButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        showLetter(currentIndex);
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex < tabs.length - 1) {
+        currentIndex++;
+        showLetter(currentIndex);
+    }
+});
+
+showLetter(currentIndex);
